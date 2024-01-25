00c11aa50ff23ff6b09f69fa61110f864d7fe74d
alfianpr <alfian.pratama@kalbenutritionals.com>
2024-01-26 01:49:36 +0700

    push

--- hackback.py
+++ hackback.py
@@ -21,7 +21,6 @@ CREDENTIAL = f"hackback_login_{args.n}"
 # Load the requirements files
 PAGE_URL = json.load(open("config/hackback_url.json"))
 PAGE_ELEMENT = json.load(open("config/hackback_element.json"))
-# CREDENTIAL_LISTS = json.load(open(f"credential/{CREDENTIAL}.cs"))
 CREDENTIAL_DF = pd.read_csv(f"credential/{CREDENTIAL}.csv")
 NEW_PASS_LISTS = json.load(open(f"credential/hackback_newpass.json"))
 
@@ -33,7 +32,7 @@ def login(email_id, password):
     try:
         open_link(PAGE_URL["url_tp"])
         log.info(f"Login to netflix {PAGE_URL['url_tp']}")
-        time.sleep(2)
+        time.sleep(7)
         filling(PAGE_ELEMENT["email_login"], email_id)
         log.info(f"Input email {email_id}")
         filling(PAGE_ELEMENT["password_login"], password)
@@ -66,17 +65,22 @@ def delete_profile(email_id):
     try:
         log.info(f"Delete the profile {email_id}")
         open_link(PAGE_URL["url_dp"])
+        time.sleep(2)
         clear_field(element=PAGE_ELEMENT["owner_profile_name"])
         clear_field(element=PAGE_ELEMENT["profile1_name"])
         clear_field(element=PAGE_ELEMENT["profile2_name"])
         clear_field(element=PAGE_ELEMENT["profile3_name"])
         clear_field(element=PAGE_ELEMENT["profile4_name"])
         filling(element=PAGE_ELEMENT["owner_profile_name"], fill="1")
+        time.sleep(3)
+        enter_key()
+        open_link(PAGE_URL["url_dp"])
+        time.sleep(2)
         filling(element=PAGE_ELEMENT["profile1_name"], fill="2")
         filling(element=PAGE_ELEMENT["profile2_name"], fill="3")
         filling(element=PAGE_ELEMENT["profile3_name"], fill="4")
         filling(element=PAGE_ELEMENT["profile4_name"], fill="5")
-        time.sleep(2)
+        time.sleep(4)
         enter_key()
     except:
         log.info(f"{email_id} already deleted")
@@ -85,7 +89,7 @@ def delete_profile(email_id):
 def remove_pin(email_id, password):
     log.info("Remove the pin")
     open_link(PAGE_URL["url_rp"])
-    time.sleep(2)
+    time.sleep(3)
     log.info(f"{email_id} : parental password")
     filling(
         element=PAGE_ELEMENT["form_parental_control"], 
@@ -123,15 +127,17 @@ def change_password(email_id, password):
     new_password = NEW_PASS_LISTS[password]
     log.info(f"Change the password for {email_id}")
     open_link(PAGE_URL["url_cp"])
-    time.sleep(4)
+    time.sleep(3)
     filling(
         element=PAGE_ELEMENT["cp_current_password"], 
         fill=password
     )
+    time.sleep(1)
     filling(
         element=PAGE_ELEMENT["cp_new_password"], 
         fill=new_password
     )
+    time.sleep(1)
     filling(
         element=PAGE_ELEMENT["cp_confirm_new_password"], 
         fill=new_password
@@ -140,23 +146,33 @@ def change_password(email_id, password):
     enter_key()
     log.info(f"Changed the password for {email_id}")
 
+# Clean the cookies
+def clean_cookies(email_id):
+    log.info("Clean the cookies")
+    open_link(PAGE_URL["url_cc"])
+    time.sleep(3)
+    log.info(f"Cleaned the cookies for {email_id}")
+
 # Flow of the scripts
 def flow():
     for _, i in CREDENTIAL_DF.iterrows():
         email_id = i["email"]
         password = i["pass"]
+
         log.info(f"Start hackback for {email_id}")
         login(email_id, password)
-        time.sleep(2)
+        time.sleep(3)
         tp_off(email_id)
         time.sleep(3)
         delete_profile(email_id)
-        time.sleep(2)
+        time.sleep(3)
         remove_pin(email_id, password)
-        time.sleep(2)
+        time.sleep(3)
         clean_history(email_id)
-        time.sleep(2)
+        time.sleep(3)
         change_password(email_id, password)
+        time.sleep(4)
+        clean_cookies(email_id)
 
 if __name__ == "__main__":
     flow()
