f9df7ac8cee874b9b36464586f613b189e2de516
alfianpr <alfian.pratama@kalbenutritionals.com>
2024-01-28 13:14:16 +0700

    push

--- hackback.py
+++ hackback.py
@@ -38,12 +38,9 @@ def login(email_id, password):
     filling(PAGE_ELEMENT["password_login"], password)
     log.info(f"Input password {password}")
     enter_key()
-    # except:
-    #     log.error(f"Cant login to netflix {email_id}")
 
 # Turn off the TP
 def tp_off(email_id):
-    # try:
     try:
         log.info(f"Turn on the TP for {email_id}")
         try:
@@ -51,18 +48,15 @@ def tp_off(email_id):
         except:
             click(PAGE_ELEMENT["tp_button_on_id"])
     except:
-        log.info(f"TP button ON for {email_id} already")
+        log.info(f"TP button OFF for {email_id} already")
     try:
         click(PAGE_ELEMENT["tp_button_done_en"])
     except:
         click(PAGE_ELEMENT["tp_button_done_id"])
     log.info("Done")
-    # except:
-    #     log.error(f"INCORRECT PASSWORD : {email_id}")
 
 # Delete the profile
 def delete_profile(email_id):
-    # try:
     log.info(f"Delete the profile {email_id}")
     open_link(PAGE_URL["url_dp"])
     time.sleep(2)
@@ -86,8 +80,6 @@ def delete_profile(email_id):
     filling(element=PAGE_ELEMENT["profile4_name"], fill="5")
     time.sleep(4)
     enter_key()
-    # except:
-    #     log.info(f"{email_id} already deleted")
 
 # Remove the pin
 def remove_pin(email_id, password):
@@ -123,6 +115,11 @@ def clean_history(email_id):
         except:
             click(PAGE_ELEMENT["hide_all_history_button_id"])
         log.info(f"Hidden the history for {email_id}")
+        time.sleep(2)
+        try:
+            click(PAGE_ELEMENT["confirm_hide_history_button_en"])
+        except:
+            click(PAGE_ELEMENT["confirm_hide_history_button_id"])
     except:
         log.info(f"{email_id} already hidden")
 
@@ -161,12 +158,23 @@ def clean_cookies(email_id):
 #     log.info(f"Get the account information for {email_id}")
 #     open_link(PAGE_URL["url_ya"])
 #     time.sleep(4)
-#     payment_method = get_text(PAGE_ELEMENT["ya_payment_method"])
-#     print(payment_method)
+#     # Payment metthod
+#     try:
+#         payment_method = get_text('//*[@id="appMountPoint"]/div/div/div/div[3]/div/div/div[5]/div[1]/section/div[2]/div/div/div[1]/div[1]')
+#     except:
+#         payment_method = get_text('//*[@id="appMountPoint"]/div/div/div/div[3]/div/div/div[5]/div[1]/section/div[2]/div/div/div[1]/div[2]')
+#     # Due date
+#     try:
+#         due_date = get_text('//*[@id="appMountPoint"]/div/div/div/div[3]/div/div/div[5]/div[1]/section/div[2]/div/div/div[1]/div[2]')
+#     except:
+#         log.error("No due date")
+#     print(payment_method, due_date)
+#     return payment_method, due_date
 
 
-# Flow of the scripts
-
+# Flow of the scripts (Alur Jalannya Script)
+# payment_method_list = []
+# due_date_list = []
 account_list = []
 status_list = []
 state_list = []
@@ -175,59 +183,65 @@ def flow():
         email_id = i["email"]
         password = i["pass"]
 
-        # Setup the driver
+        # Setup the driver (Opening the browser)
         driver_start()
         try:
             log.info(f"Start hackback for {email_id}")
-            login(email_id, password)
+            login(email_id, password) # Login to netflix
         except:
             log.error(f"FAILED LOGIN : {email_id}")
             status = "FAILED"
             state = "failed login"
         try:
             time.sleep(3)
-            tp_off(email_id)
+            tp_off(email_id) # Turn off the TP
         except:
-            log.error(f"FAILED TP : {email_id}")
+            log.error(f"FAILED turn off the TP : {email_id}")
             status = "FAILED"
-            state = "failed tp"
+            state = "failed turn off the tp"
         try:
             time.sleep(3)
-            delete_profile(email_id)
+            delete_profile(email_id) # Delete the profile
         except:
             log.error(f"FAILED DELETE : {email_id}")
             status = "FAILED"
             state = "failed delete profile"
         try:
             time.sleep(3)
-            remove_pin(email_id, password)
+            remove_pin(email_id, password) # Remove the pin
         except:
             log.error(f"FAILED PIN : {email_id}")
             status = "FAILED"
             state = "failed remove pin"
         try:
             time.sleep(3)
-            clean_history(email_id)
+            clean_history(email_id) # Clean the history
         except:
             log.error(f"FAILED HISTORY : {email_id}")
             status = "FAILED"
             state = "failed clean history"
         try:
             time.sleep(3)
-            change_password(email_id, password)
+            change_password(email_id, password) # Change the password
         except:
             log.error(f"FAILED PASSWORD : {email_id}")
             status = "FAILED"
             state = "failed change password"
         try:
             time.sleep(4)
-            clean_cookies(email_id)
+            clean_cookies(email_id) # Clean the cookies
         except:
             log.error(f"FAILED CLEAN COOKIES : {email_id}")
             status = "FAILED"
             state = "failed clean cookies"
 
-        account_list.append(email_id)
+        # Create information log, save to the results file
+        try:
+            payment_method, due_date = get_account_information(email_id)
+        except:
+            log.error(f"FAILED GET ACCOUNT INFORMATION : {email_id}")
+            status = "FAILED"
+            state = "failed get account information"
         try:
             status_list.append(status)
         except:
@@ -236,6 +250,14 @@ def flow():
             state_list.append(state)
         except:
             state_list.append("success")
+        # try:
+        #     payment_method_list.append(payment_method)
+        # except:
+        #     payment_method_list.append("Failed")
+        # try:
+        #     due_date_list.append(due_date)
+        # except:
+        #     due_date_list.append("Failed")
 
         df_result = pd.DataFrame(
             {
@@ -249,7 +271,7 @@ def flow():
             f"results/hackback_{args.n}.csv"
         )
 
-        close_driver()
+        close_driver() # Close the driver
 
         log.info(f"Done hackback for {email_id}")
 if __name__ == "__main__":
