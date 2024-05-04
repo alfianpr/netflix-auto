2fe4a2271e7caa46a0cdcd313ad0e7678f1184d1
alfianpr <alfianpratama@hotmail.com>
2024-05-04 23:37:56 +0700

    update

---
+++ cp2.py
@@ -0,0 +1,3 @@
+from netlib.cplib import (
+    
+)
--- netlib/netlib/cplib.py
+++ netlib/netlib/cplib.py
@@ -28,3 +28,32 @@ driver = webdriver.Chrome(
 )
 action = ActionChains(driver)
 
+#/////////////////////// FUNCTION /////////////////////////////
+def Click(element):
+    driver.find_element(by = 'xpath', value = element).click()
+
+def Filling(element, fill):
+    field = driver.find_element(by = 'xpath', value = element)
+    field.send_keys(fill)
+
+def GetText(element):
+    time.sleep(2)
+    return driver.find_element(by = 'xpath', value = element).text
+
+def ClearField(element):
+    field = driver.find_element(by = 'xpath', value = element)
+    field.send_keys(Keys.CONTROL + "a")
+    field.send_keys(Keys.DELETE)
+
+def ScrollDown(value = 1) :
+    page = driver.find_element(by = 'tag name', value = 'html')  
+    for scroll in range(value):  
+        page.send_keys(Keys.ARROW_DOWN) 
+
+def logging(text):
+    print(text)
+    log_file.write(text)
+    log_file.write('\n')
+def ClearCookies():
+    driver.get(url_cc)
+    time.sleep(3)
